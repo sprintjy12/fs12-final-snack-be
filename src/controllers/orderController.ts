d9b72afebf1046053456bf7008767d3666ca0268
sebikawa32 <tpqlsdl150@naver.com>
2026-07-28 10:12:33 +0900

    edit: 응답형식 일치

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -5,36 +5,40 @@ import OrderRepository from "../repositories/orderRepository";
 
 const orderService = new OrderService(new OrderRepository());
 
-export const getOrderHistoryList = asyncHandler(async (req: Request, res: Response) => {
-  const userId = req.user!.id;
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const sort = (req.query.sort as "latest" | "highPrice" | "lowPrice") || "latest";
+export const getOrderHistoryList = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user!.id;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const sort =
+      (req.query.sort as "latest" | "highPrice" | "lowPrice") || "latest";
 
-  const result = await orderService.getOrderHistory({
-    userId,
-    page,
-    limit,
-    sort,
-  });
+    const result = await orderService.getOrderHistory({
+      userId,
+      page,
+      limit,
+      sort,
+    });
 
-  return res.status(200).json({
-    success: true,
-    message: "구매 내역 리스트 조회 성공",
-    ...result,
-  });
-});
+    return res.status(200).json({
+      success: true,
+      message: "구매 내역 리스트 조회 성공",
+      data: result,
+    });
+  },
+);
 
-export const getOrderHistoryDetail = asyncHandler(async (req: Request, res: Response) => {
-  const userId = req.user!.id;
-  const orderId = req.params.orderId as string;
+export const getOrderHistoryDetail = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user!.id;
+    const orderId = req.params.orderId as string;
 
-  const data = await orderService.getOrderDetail(orderId, userId);
+    const data = await orderService.getOrderDetail(orderId, userId);
 
-  return res.status(200).json({
-    success: true,
-    message: "구매 내역 상세 조회 성공",
-    data,
-  });
-});
- 
+    return res.status(200).json({
+      success: true,
+      message: "구매 내역 상세 조회 성공",
+      data,
+    });
+  },
+);

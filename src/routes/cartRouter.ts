b15882f411a20277e35dfda3964213a6820e64d1
김상우 <kksw7000@gmail.com>
2026-07-29 16:49:23 +0900

    edit: 코드래빗 수정

--- src/routes/cartRouter.ts
+++ src/routes/cartRouter.ts
@@ -1,30 +1,29 @@
 import express from "express";
+import cartController from "../controllers/cartController";
 
-const cartController = express.Router();
+const cartRouter = express.Router();
 
 // 장바구니 조회
-cartController.get("/", (req, res) => {
-  return res.status(200).json({ message: "장바구니 조회 성공" });
-});
+cartRouter.get("/", cartController.getCart);
 
 // 장바구니 추가
-cartController.post("/", (req, res) => {
+cartRouter.post("/", (req, res) => {
   return res.status(200).json({ message: "장바구니 추가 성공" });
 });
 
 // 장바구니 수량 수정
-cartController.patch("/:cartId", (req, res) => {
+cartRouter.patch("/:cartId", (req, res) => {
   return res.status(200).json({ message: "장바구니 수량 수정 성공" });
 });
 
 // 장바구니  개별 삭제
-cartController.delete("/:cartId", (req, res) => {
+cartRouter.delete("/:cartId", (req, res) => {
   return res.status(200).json({ message: "장바구니 삭제 성공" });
 });
 
 // 장바구니 전체 삭제
-cartController.delete("/", (req, res) => {
+cartRouter.delete("/", (req, res) => {
   return res.status(200).json({ message: "장바구니 전체 삭제 성공" });
 });
 
-export default cartController;
+export default cartRouter;
--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -9,38 +9,38 @@ import {
   productIdParamSchema,
 } from "../schemas/productSchema.js";
 
-const productRoutes = express.Router();
+const productRouter = express.Router();
 
 // 상품 전체 조회 -
-productRoutes.get(
+productRouter.get(
   "/",
   validate(getProductsQuerySchema, "query"),
   asyncHandler(productController.getProducts),
 );
 
 // 상품 등록 -
-productRoutes.post(
+productRouter.post(
   "/",
   validate(createProductSchema, "body"),
   asyncHandler(productController.createProduct),
 );
 
 // 내가 등록한 상품 조회 -
-productRoutes.get(
+productRouter.get(
   "/me",
   validate(getProductsQuerySchema, "query"),
   asyncHandler(productController.getMyProducts),
 );
 
 // 상품 상세 조회 -
-productRoutes.get(
+productRouter.get(
   "/:productId",
   validate(productIdParamSchema, "params"),
   asyncHandler(productController.getProductById),
 );
 
 // 상품 수정 -
-productRoutes.patch(
+productRouter.patch(
   "/:productId",
   validate(productIdParamSchema, "params"),
   validate(updateProductSchema, "body"),
@@ -48,10 +48,10 @@ productRoutes.patch(
 );
 
 // 상품 삭제 -
-productRoutes.delete(
+productRouter.delete(
   "/:productId",
   validate(productIdParamSchema, "params"),
   asyncHandler(productController.deleteProduct),
 );
 
-export default productRoutes;
+export default productRouter;
